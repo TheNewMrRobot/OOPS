a0c209eb06f58a02bff6c7c425219952992f2d00
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-03-26 15:53:35 +0530

    addding default value to quantity is provided it will assign the default value

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class Item:
-    def __init__(self,name,price,quantity):
+    def __init__(self,name,price,quantity=0):
         self.name = name
         self.price = price
         self.quantity = quantity
     def calculateTotal(self,x,y):
         return x * y
     
-item = Item("phone",100000,2)
+item = Item("phone",100000)
