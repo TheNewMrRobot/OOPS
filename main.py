3c19951cf29ddcc671e2e6777509817d45cc0730
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-03-26 16:04:59 +0530

    added to list when each item is created

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 class Item:
     # if want to give discount for all items so
     dis_rate = 0.7
+    all = []
     def __init__(self,name: str, price: float,quantity=0):
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.all.append(self)
     def calculateTotal(self):
         return self.price * self.quantity
     def applyDiscount(self):
@@ -13,5 +15,11 @@ class Item:
         return self.price * Item.dis_rate
     
 item = Item("phone",1000,2)
-print(item.calculateTotal())
+item2 = Item("phone",1000,2)
+item3 = Item("phone",1000,2)
+item4 = Item("phone",1000,2)
+item5 = Item("phone",1000,2)
+
+print(Item.all)
+# print(item.calculateTotal())
 # item.dis_rate = 0.5
