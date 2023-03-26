f0fbc6e784ab3af2dbdf12c5acc53193fce3add3
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-03-26 16:10:40 +0530

    fixed apply discount and calculate total

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Item:
     def applyDiscount(self):
         # if we use Item then it will take the defiend one in class
         # if we specify self.dis_rate that will take the discount value from the object
-        return self.price * Item.dis_rate
+        self.price = self.price * Item.dis_rate
     
     def __repr__(self) -> str:
         return f"Item({self.name},{self.price},{self.quantity})"
@@ -26,5 +26,8 @@ item5 = Item("phone",1000,2)
 
 # problem is we can see the object location memory thats not readable so use __rep
 print(Item.all)
+
+item.applyDiscount()
+print(item.calculateTotal())
 # print(item.calculateTotal())
 # item.dis_rate = 0.5
