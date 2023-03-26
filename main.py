1ac0d960966091c1fe344a02f508aafd8cbbf0d1
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-03-26 16:07:19 +0530

    added to list and converted to readable by __repr__ method

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ class Item:
         # if we specify self.dis_rate that will take the discount value from the object
         return self.price * Item.dis_rate
     
+    def __repr__(self) -> str:
+        return f"Item({self.name},{self.price},{self.quantity})"
+    
 item = Item("phone",1000,2)
 item2 = Item("phone",1000,2)
 item3 = Item("phone",1000,2)
 item4 = Item("phone",1000,2)
 item5 = Item("phone",1000,2)
 
+# problem is we can see the object location memory thats not readable so use __rep
 print(Item.all)
 # print(item.calculateTotal())
 # item.dis_rate = 0.5
